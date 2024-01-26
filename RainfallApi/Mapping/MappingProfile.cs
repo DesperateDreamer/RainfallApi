40f8550b2ffbd8022723761a3d764531b29985c8
Ivanka Kirinova <77276607+DesperateDreamer@users.noreply.github.com>
2024-01-26 18:12:46 +0200

    Add and setup Automapper

---
+++ RainfallApi/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RainfallApi.Models;
+using RainfallApi.Responses;
+
+namespace RainfallApi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ReadingApiModel, RainfallReadingResponse>()
+                .ForMember(dest => dest.Readings, src => src.MapFrom(x => x.Items))
+                .ReverseMap();
+
+            CreateMap<ReadingApiItemModel, RainfallReading>()
+                .ForMember(dest => dest.DateMeasured, src => src.MapFrom(x => x.DateTime))
+                .ForMember(dest => dest.AmountMeasured, src => src.MapFrom(x => x.Value))
+                .ReverseMap();
+        }
+    }
+}
