3856276b3ef170774b147bdfe32a2fff2adc430b
Ivanka Kirinova <77276607+DesperateDreamer@users.noreply.github.com>
2024-01-26 17:40:31 +0200

    Setup HttpClientFactory

---
+++ RainfallApi/Services/RainfallService.cs
@@ -0,0 +1,14 @@
+﻿namespace RainfallApi.Services
+{
+    public class RainfallService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RainfallService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+    }
+}
